5e22043d7b7a97b84e145f0198d4ae7f34d973b8
Камильянов Рустем Ильдарович <rustem.kamilyanov@OCRV.RU>
2021-11-14 13:27:36 +0300

    gnn.ipynb model need fix

--- defectoscopy_RNN/trash.py
+++ defectoscopy_RNN/trash.py
@@ -1,9 +1,1 @@
-import os
-
-train_preproc_path = 'C:/Users/rustem.kamilyanov/defectoscopy2/train/preproc'
-test_preproc_path = 'C:/Users/rustem.kamilyanov/defectoscopy2/test/preproc'
-
-train_names = os.listdir(train_preproc_path)
-test_names = os.listdir(test_preproc_path)
-
-print(train_names)
+import torch_geometric.nn
